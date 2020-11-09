46aba516b3bfe4019702599e9f01ee246df0d2b5
Puleeno Nguyen <puleeno@gmail.com>
2020-11-09 11:04:11 +0700

    Create base plugin

---
+++ includes/Phocarolary.php
@@ -0,0 +1,5 @@
+<?php
+namespace Jankx\Phocarolary;
+
+class Phocarolary {
+}
---
+++ phocarolary.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Phocarolary
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Description: Create photo carousel and gallery for Jankx framework
+ * Version: 1.0.0
+ */
+
+define('PHOCAROLARY_PLUGIN_FILE', __FILE__);
